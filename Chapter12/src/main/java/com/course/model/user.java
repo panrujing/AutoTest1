05c9b41f81016608099eddca0dddd44027d168da
panrujing <panrujing123@163.com>
2019-11-20 18:06:21 +0800

    测试12

---
+++ Chapter12/src/main/java/com/course/model/user.java
@@ -0,0 +1,12 @@
+package com.course.model;
+
+/**
+ * created by panrujing on 2019/11/20.
+ */
+public class user {
+    private int id;
+    private String username;
+    private String age;
+    private String sex;
+
+}
